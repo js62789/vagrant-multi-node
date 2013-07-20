641b6f2b2ff2137e95314f88c62a49f85fc38683
Jeffrey Smith <js62789@gmail.com>
2013-07-19 21:44:41 -0400

    Express, socket, jade, jquery, normalize

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,17 +5,56 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
-      files: 'src/js/**/*.js'
+      files: 'src/js/**/*.js',
+      options: {
+
+        // Global vars that can be used freely without definition in each file
+        globals: {
+          _:          true,
+          $:          true,
+          Handlebars: true,
+          gila:       true,
+          FastClick:  true,
+          Backbone:   true,
+          Marionette: true,
+          App:        true
+        },
+
+        // Options
+        eqeqeq:    true,     // Require !== and ===
+        immed:     true,     // Make sure immediate function invocations are wrapped like (function(){...})()
+        latedef:   true,     // Variables must be defined before they're used
+        maxdepth:  2,        // Limit the amount of nesting within a block
+        maxparams: 3,        // Limit number of function arguments
+        newcap:    true,     // Constructor names must start with a capital letter
+        noarg:     true,     // Prevent arguments.caller & arguments.callee
+        quotmark:  'single', // Require single quotation marks
+        trailing:  true,     // Prevent trailing whitespace
+        undef:     true,     // Prohibit undefined variables
+        unused:    'vars',   // Check for unused variables
+
+        // Relaxing options
+        sub:       true,     // Suppress warning about using person['name'] instead of person.name
+
+        // Environments
+        browser:   true,     // Define globals exposed by the browser (document, navigator, etc)
+        devel:     true,     // Define dev globals exposed by the browser (console, alert, etc)
+        jquery:    true      // Define globals exposed by jQuery
+      }
+    },
+
+    less: {
+      app: {
+        files: {
+          'public/css/<%= pkg.name %>.css': 'src/styles/app.less'
+        }
+      }
     },
     
     concat: {
       js: {
         src: 'src/js/**/*.js',
         dest: 'public/js/<%= pkg.name %>.js'
-      },
-      css: {
-        src: 'src/css/**/*.css',
-        dest: 'public/css/<%= pkg.name %>.css'
       }
     },
     
@@ -51,10 +90,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
-  grunt.registerTask('dev', ['jshint', 'concat', 'uglify', 'cssmin', 'watch']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'less', 'cssmin']);
+  grunt.registerTask('dev', ['jshint', 'concat', 'uglify', 'less', 'cssmin', 'watch']);
 
 };
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 var http = require('http')
+  , express = require('express')
+  , socket = require('socket.io')
   , os   = require('os')
+  , app = module.exports = express()
+  , server = http.createServer(app)
+  , io = socket.listen(server)
   , hostname = os.hostname()
   , port = 3000;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from ' + hostname + '\n');
-}).listen(port);
+app.configure(function () {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {layout: false});
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
 
-console.log('Server running at http://127.0.0.1:' + port + '/');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+  res.render('index', {"hostname": hostname});
+});
+
+server.listen(port, function () {
+  console.log('Server running at http://127.0.0.1:' + port + '/');
+});
